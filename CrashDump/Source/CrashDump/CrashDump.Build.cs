bad4709915a637ff967b09baa608c0cd639fbc10
Junichi Kimura <junichi@netspace.org>
2019-04-10 16:22:13 -0700

    Make the plugin build under UE4.20

--- CrashDump/Source/CrashDump/CrashDump.Build.cs
+++ CrashDump/Source/CrashDump/CrashDump.Build.cs
@@ -10,19 +10,23 @@ public class CrashDump : ModuleRules
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 		
-		PublicIncludePaths.AddRange(new string[] {"CrashDump/Public"});
-		PrivateIncludePaths.AddRange(new string[] {"CrashDump/Private"});		
-		PublicDependencyModuleNames.AddRange(new string[]{"Core", "CoreUObject","Engine"});
+		PublicDependencyModuleNames.AddRange(
+			new string[] {
+					"Core",
+					"CoreUObject",
+					"Engine"
+            });
 
         if (Target.Platform == UnrealTargetPlatform.Android && 
             Target.Configuration != UnrealTargetConfiguration.Shipping &&
             Target.Configuration != UnrealTargetConfiguration.Test)
         {
-            Definitions.Add("WITH_CRASH_DUMP=1");
+            PublicDefinitions.Add("WITH_CRASH_DUMP=1");
             PrivateDependencyModuleNames.AddRange(new string[] { "Launch" });
 
-            string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, BuildConfiguration.RelativeEnginePath);
-            AdditionalPropertiesForReceipt.Add(new ReceiptProperty("AndroidPlugin", Path.Combine(PluginPath, "CrashDump_UPL_Android.xml")));
+            string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+            AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(PluginPath, "CrashDump_UPL_Android.xml"));
+
 
             string ThirdPartyPath = Path.Combine(ModuleDirectory, "..", "ThirdParty");
             PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "GoogleBreakpad", "include"));
@@ -30,7 +34,7 @@ public class CrashDump : ModuleRules
         }
         else
         {
-            Definitions.Add("WITH_CRASH_DUMP=0");
+            PublicDefinitions.Add("WITH_CRASH_DUMP=0");
         }
 
     }
